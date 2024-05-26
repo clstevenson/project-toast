3e5d181abd0ef0f3e87c37ecea2c513af92ae0d0
Chris Stevenson <chrislstevenson@gmail.com>
2024-05-26 18:45:09 +0300

    Design and test Toast component API
    - before looking at soln vid

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -4,11 +4,18 @@ import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
 
+import Toast from '../Toast';
+
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState('');
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [toastDisplay, setToastDisplay] = React.useState(false);
+
+  const handleClose = () => {
+    setToastDisplay((prev) => !prev);
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +23,8 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {toastDisplay && <Toast message={message} variant={variant} handleClose={handleClose}/>}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -65,7 +74,14 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button>Pop Toast!</Button>
+            <Button
+              onClick={() => {
+                const display = !toastDisplay;
+                setToastDisplay((prev) => !prev)
+              }}
+            >
+              Pop Toast!
+            </Button>
           </div>
         </div>
       </div>
